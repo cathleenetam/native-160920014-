452a6f784f7d8bfcda17c20dfa436672971e6909
cathleenetam <s160920014@student.ubaya.ac.id>
2024-02-22 17:21:21 +0700

    registration activity wip

--- app/src/main/java/com/example/native160920014/RegistrationActivity.kt
+++ app/src/main/java/com/example/native160920014/RegistrationActivity.kt
@@ -3,11 +3,9 @@ package com.example.native160920014
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-
-
+        setContentView(R.layout.activity_registration)
     }
 }
